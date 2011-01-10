ab5a3e17eb252e6888822b54f7f85dbc9939bad5
sassman <sven.assmann@lubico.biz>
2011-01-10 19:26:39 +0100

    update schema

--- config/schema/install.sql
+++ config/schema/install.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `monitoring_objects` (
   `id` char(36) NOT NULL,
   `model` varchar(250) NOT NULL,
-  `foreign_key` char(36) NOT NULL,
+  `foreign_key` varchar(36) NOT NULL,
   `type` varchar(100) NOT NULL,
   `visitor_id` char(36) default NULL,
   `ip` varchar(15) default NULL,
